5b67d5cc9602de0ad4fc97318e6c6b9cef8402ca
Aravindh <112405729+wizzysss@users.noreply.github.com>
2023-02-24 18:13:06 +0000

    22.16

--- ToWatch/frontent/src/modals/CreateTask.js
+++ ToWatch/frontent/src/modals/CreateTask.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const CreateTask = ({modal,toggle}) => {
+  const[taskName, setTaskName] = useState('');
+  const [description, setDescription] =useState('');
     return (
-        <div>
-            <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Add your anime</ModalHeader>
+        <div >
+            <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle} >/ADD YOUR ANIME</ModalHeader>
         <ModalBody>
          <form>
-
+             <div className='form-group'>
+              <label className='sauce'>Sauce</label>
+              <input type="text" className='animename form-control' value ={taskName}/>
+             </div>
+             <div className='form-group'>
+              <label className='sauce'>description</label>
+              <textarea rows ="3" className='sauce form-control' value ={description}></textarea>
+             </div>
          </form>
         </ModalBody>
         <ModalFooter>
