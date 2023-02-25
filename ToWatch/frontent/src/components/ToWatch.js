eb8e9cbbbf58a493aa292a50bcc2ca13faf610eb
Aravindh <112405729+wizzysss@users.noreply.github.com>
2023-02-25 05:22:25 +0000

    listed the todo part 1 video is over

--- ToWatch/frontent/src/components/ToWatch.js
+++ ToWatch/frontent/src/components/ToWatch.js
@@ -3,18 +3,28 @@ import CreateTask from '../modals/CreateTask';
 
 const ToWatch = () => {
     const [modal, setModal] = useState(false);
-    
+    const [taskList,settaskList] = useState([])
+
     const toggle = () => {
         setModal(modal);
     }
-    
+    const saveTask = (taskObj) => {
+        let tempList =  taskList
+        tempList.push(taskObj)
+        settaskList(tempList)
+        setModal(false)
+    }
+
     return (
         <>
         <div className='Header text-center'>
             <h1 className='title'>/ToWatch</h1>
             <button className='btn btn-light mt-3' onClick = {() => setModal(true)}>Click Here</button>
         </div>
-        <CreateTask toggle = {toggle} modal ={modal}/>
+        <div className='task-container'>
+{taskList.map((obj) => <li>{obj.Name}</li>)}
+            </div>
+        <CreateTask toggle = {toggle} modal ={modal} save ={saveTask}/>
         </>
     );
 };
--- ToWatch/frontent/src/modals/CreateTask.js
+++ ToWatch/frontent/src/modals/CreateTask.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTask = ({modal,toggle}) => {
+const CreateTask = ({modal,toggle,save}) => {
   const[taskName, setTaskName] = useState('');
   const [description, setDescription] =useState('');
+
+  const handleChange = (e) => {
+ const {name,value}= e.target
+ if (name==="taskName"){
+  setTaskName(value)
+ }else{
+  setDescription(value)
+ }
+  }
+
+
+  const handleSave = () => {
+      let taskObj ={}
+      taskObj["Name"]=taskName
+      taskObj["Description"]=description
+      save(taskObj)
+  }
     return (
         <div >
             <Modal isOpen={modal} toggle={toggle} >
@@ -12,16 +29,16 @@ const CreateTask = ({modal,toggle}) => {
          <form>
              <div className='form-group'>
               <label className='sauce'>Sauce</label>
-              <input type="text" className='animename form-control' value ={taskName}/>
+              <input type="text" className='animename form-control' value ={taskName} onChange = {handleChange} name ="taskName"/>
              </div>
              <div className='form-group'>
               <label className='sauce'>description</label>
-              <textarea rows ="3" className='sauce form-control' value ={description}></textarea>
+              <textarea rows ="3" className='sauce form-control' value ={description} onChange = {handleChange} name ="description"></textarea>
              </div>
          </form>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
+          <Button color="primary" onClick={handleSave}>
             Create
           </Button>{' '}
           <Button color="secondary" onClick={toggle}>
